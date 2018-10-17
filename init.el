a207e7871e8b332e7c839b4a5af2e2cced602089
Evgeniy Kochetov <evgeniik@mellanox.com>
2018-10-17 12:05:30 +0000

    Some work related settings

--- init.el
+++ init.el
@@ -16,10 +16,16 @@
     (("gnu" . "https://elpa.gnu.org/packages/")
      ("melpa" . "https://melpa.org/packages/")
      ("org" . "https://orgmode.org/elpa/"))))
- '(package-selected-packages (quote (org))))
+ '(package-selected-packages (quote (org)))
+ '(c-basic-offset 8)
+ '(column-number-mode t)
+ '(menu-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(global-whitespace-mode t)
+(show-paren-mode 1)
